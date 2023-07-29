17f468a0449c2d6fd3a1eaf9fcf7e4069336d1c7
Yun Ye <yunye.koo10@rafflesgirlssch.edu.sg>
2023-07-29 11:13:08 +0800

    Initial Commit

---
+++ todo 2/todo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  todo_2App.swift
+//  todo 2
+//
+//  Created by Koo  on 29/7/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
